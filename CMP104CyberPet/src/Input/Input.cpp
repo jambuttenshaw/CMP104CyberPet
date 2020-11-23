6424d85e2c29c05ff81d714203e7db355ce791fb
Jamie Buttenshaw <sigton.scratch@gmail.com>
2020-11-23 16:34:13 +0000

    Creating event polling system

--- CMP104CyberPet/src/Input/Input.cpp
+++ CMP104CyberPet/src/Input/Input.cpp
@@ -1,5 +1,7 @@
 #include "Input.h"
 
+#include <iostream>
+
 Input::Input()
 {
 	// assign the console handle
@@ -9,6 +11,15 @@ Input::Input()
 	GetConsoleMode(m_ConsoleHandle, &m_OldConsoleMode); // save the old mode
 	DWORD consoleMode = ENABLE_WINDOW_INPUT | ENABLE_MOUSE_INPUT;
 	SetConsoleMode(m_ConsoleHandle, consoleMode);
+
+
+	std::vector<INPUT_RECORD> m_InputRecordBuffer(128);
+}
+
+void Input::Poll()
+{
+	ReadConsoleInput(m_ConsoleHandle, &m_InputRecordBuffer[0], 128, &m_NumInputRecords);
+	std::cout << "Num events read into buffer: " << m_NumInputRecords << std::endl;
 }
 
 Input::~Input()
--- CMP104CyberPet/src/Input/Input.h
+++ CMP104CyberPet/src/Input/Input.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <vector>
 #include <windows.h>
 
 class Input
@@ -8,10 +9,16 @@ public:
 	Input();
 	~Input();
 
-private:
-	HANDLE m_ConsoleHandle = nullptr;
+	void Poll();
 
+private:
+	// handle to the console
+	HANDLE m_ConsoleHandle = nullptr;
 	// save the old console mode so we can revert back upon program end
 	DWORD m_OldConsoleMode = 0;
+
+	// a buffer to hold input records that have not yet been handled
+	std::vector<INPUT_RECORD> m_InputRecordBuffer; // more than large enough to hold all the inputs that will need processed in a single frame
+	DWORD m_NumInputRecords = 0; // the number of input records to be processed this frame
 };
 
