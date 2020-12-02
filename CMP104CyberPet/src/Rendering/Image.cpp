bac3771ab388faad8f7afeb3c49b4682298ebb5b
Jamie Buttenshaw <sigton.scratch@gmail.com>
2020-12-02 14:45:42 +0000

    commented the image class

--- CMP104CyberPet/src/Rendering/Image.cpp
+++ CMP104CyberPet/src/Rendering/Image.cpp
@@ -1,23 +1,30 @@
 #include "Image.h"
 
+// the first constructor
+// create the image using the default fill character
 Image::Image(int width, int height, wchar_t defaultFill)
 	: m_Width(width), m_Height(height)
 {
 	CreateImage(defaultFill);
 }
 
+// the next constructor creates the image out of the given string
 Image::Image(int width, int height, const std::string& imageContent)
 	: m_Width(width), m_Height(height)
 {
 	CreateImage(imageContent);
 }
 
+// the copy constructor: creates a deep copy of the image given
+// when we create a new image out of an old one we do not want both objects
+// pointing to the same image in memory
 Image::Image(const Image& oldImage)
 	: m_Width(oldImage.m_Width), m_Height(oldImage.m_Height)
 {
 	CopyImage(oldImage.m_ImageData);
 }
 
+// the destructor frees the image data from memory
 Image::~Image()
 {
 	// free each sub-array in the 2d array of image data
@@ -26,6 +33,7 @@ Image::~Image()
 	delete[] m_ImageData;
 }
 
+// create image out of a default fill character
 void Image::CreateImage(wchar_t defaultFill)
 {
 	// an image is just a 2d array of chars
@@ -35,6 +43,7 @@ void Image::CreateImage(wchar_t defaultFill)
 	// so here ill create a an array of pointers, where each pointer points to another array
 	// and voila; a 2d array
 
+	// create an array of pointers with the size of the height of the image
 	m_ImageData = new wchar_t* [m_Height];
 	for (int i = 0; i < m_Height; i++)
 	{
@@ -49,9 +58,11 @@ void Image::CreateImage(wchar_t defaultFill)
 	}
 }
 
+// this creates the image out of the string
 void Image::CreateImage(const std::string& imageContent)
 {
 	// initialize the image data array with empty arrays
+	// create an arary of pointers with the size of the height of the image
 	m_ImageData = new wchar_t* [m_Height];
 	for (int i = 0; i < m_Height; i++)
 	{
@@ -62,19 +73,24 @@ void Image::CreateImage(const std::string& imageContent)
 
 	// image is created largely the same way,
 	// except we access the string to fill in the image rather than just using a default fill character
+
+	// the coordinates we are about to place a character at in the 2D array
 	int x = 0;
 	int y = 0;
 
-
+	// convert it to a c string: a pointer to the first character in the string
 	const char* cString = imageContent.c_str();
 
+	// iterate for the length of the string
 	for (int i = 0; i < strlen(cString); i++)
 	{
 		// we do not want to put newlines into the char array
 		if (cString[i] == '\n')
 			continue;
 
-		// place the current character in the array
+		// place the current character from the string in the array
+		// we need to cast it to a wchar_t cause that is the data type
+		// the console api works with
 		m_ImageData[y][x] = (wchar_t)cString[i];
 
 		// increase x
@@ -82,13 +98,14 @@ void Image::CreateImage(const std::string& imageContent)
 		// if we have reached the end of the row, move onto the next one
 		if (x == m_Width)
 		{
+			// move back to the first column and move down a row
 			x = 0;
 			y++;
 		}
 	}
 }
 
-
+// copies an image when a deep copy of an image object is required
 void Image::CopyImage(wchar_t** imageData)
 {
 	// initialize the image data array with empty arrays
@@ -99,6 +116,8 @@ void Image::CopyImage(wchar_t** imageData)
 		m_ImageData[i] = new wchar_t[m_Width];
 	}
 
+	// just sample the old image for whatever character existed there,
+	// and place it at the same position in the new array
 	for (int i = 0; i < m_Height; i++)
 	{
 		for (int j = 0; j < m_Width; j++)
@@ -108,7 +127,7 @@ void Image::CopyImage(wchar_t** imageData)
 	}
 }
 
-
+// set a specific character at a specific position
 void Image::SetCharacter(int x, int y, wchar_t character)
 {
 	m_ImageData[y][x] = character;
--- CMP104CyberPet/src/Rendering/Image.h
+++ CMP104CyberPet/src/Rendering/Image.h
@@ -4,31 +4,61 @@
 
 #include "../Core/MathsUtility.h"
 
+// a class that is used for displaying graphics on the console
+// since we can only display characters on the console,
+// an 'image' here is simply a 2D array of characters
+
+// windows console api requires the data to be of type wchar_t; wide character
+// a character that takes up 2 bytes instead of 1.
+
 class Image
 {
 public:
 	// constructors
+	// either create an image filled from 1 character,
+	// or created from a string, where each line of the string
+	// is each row in the image
 	Image(int width, int height, wchar_t defaultFill = ' ');
 	Image(int width, int height, const std::string& imageContent);
+	// a copy constructor:
+	// important when we need to assign a sprite the image of another sprite
+	// but we dont want both sprites pointing towards the same data in memory
+	// because when one sprite gets deleted, the other sprite will no longer
+	// have an image to display on screen
 	Image(const Image& oldImage);
+	
+	// image destructor
 	~Image();
 
+	// simple getters to return the width and height of the image
+	// or get the dimensions as a Vector
 	inline int GetWidth() { return m_Width; }
 	inline int GetHeight() { return m_Height; }
 	inline Vector2i GetDimensions() { return Vector2i(m_Width, m_Height); }
 
+	// get the actual data in the image
+	// return a pointer to the 2D array
 	inline wchar_t** GetImageData() { return m_ImageData; }
 
+	// set a specific character in the image
+	// specify the x and y coordinates within the image, and the character you want to place
 	void SetCharacter(int x, int y, wchar_t character);
 
 private:
+	// private functions used to create the image out of the data supplied in the constructor
+	
+	// this one creates an image fill with 1 single character
 	void CreateImage(wchar_t defaultFill);
+	// this one creates an image from a string, where each line in the string is a row in the image
 	void CreateImage(const std::string& imageContent);
+	// this one recreates a given image but in a new place in memory; for copying an image
 	void CopyImage(wchar_t** imageData);
 
 private:
+	// the width and height of the image
 	int m_Width, m_Height;
 
+	// the actual data of the image
 	wchar_t** m_ImageData;
 
 };
