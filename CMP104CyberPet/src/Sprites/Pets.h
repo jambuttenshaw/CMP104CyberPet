b294d09744bdb81231024ce7de6770111b375c25
Jamie Buttenshaw <sigton.scratch@gmail.com>
2020-12-04 11:41:54 +0000

    more commenting

--- CMP104CyberPet/src/Sprites/Pets.h
+++ CMP104CyberPet/src/Sprites/Pets.h
@@ -2,30 +2,59 @@
 
 #include "../Core/Sprite.h"
 
+// a generic parent class for one of the pet options
+// it inherits from sprite as it has a position and an image
 class Pet : public Sprite
 {
 public:
+	// call the sprite constructor
 	Pet()
 		: Sprite()
 	{}
 
+	// we want to call the base class implementation of Update
+	// as that is the code that actually Lerps the sprite across screen
 	void Update(float deltaTime) override 
 	{
 		Sprite::Update(deltaTime); 
 	};
 
+	// move on screen will set up the linear interpolation from off screen to the center of the screen
+	// then as time passes the sprite will move into position,
+	// which is all handled in Sprite::Update
 	void MoveOnScreen()
 	{
+		// set the position as off screen
 		SetCentrePosition({ 32, -12 });
+		// set the desired position to the centre of the screen
+		// LerpToPosition takes in the topleft corner of the image, so we have to adjust for differing image dimensions
 		LerpToPosition({ 32 - (GetImage()->GetWidth() * 0.5f), 8 - (GetImage()->GetHeight() * 0.5f) });
 	}
 
+	// move off screen sets up the lerping from the centre of the screen to off to the left
 	void MoveOffScreen()
 	{
+		// make sure the sprite is positioned correctly in the centre of the screen
 		SetCentrePosition({ 32, 8 });
+		// then set the lerping to move it off to the left
 		LerpToPosition({ -32 - (GetImage()->GetWidth() * 0.5f), 8 - (GetImage()->GetHeight() * 0.5f) });
 	}
 };
+
+
+/*
+Each pet can be easily defined by inheriting from the Pet class
+and then assigning the pet a unique image
+
+when creating the image, the width and height of the image has to be passed in as wel as the string of the content of the image
+
+when setting the centre position of a sprite, you need to already have an image as the dimensions of the image are
+used to determine where the centre of the sprite is
+
+
+Each child class represents a different pet choice
+I designed this to be as easy as possible to add more pet choices
+*/
 
 
 class Frog : public Pet
--- CMP104CyberPet/src/Sprites/VisualEffectSprites.h
+++ CMP104CyberPet/src/Sprites/VisualEffectSprites.h
@@ -2,9 +2,19 @@
 
 #include "../Core/Sprite.h"
 
+/*
+A VisualEffectSprite is a sprite
+whos only purpose is to appear at a position,
+and then move to another position
+
+it is very effective at making cool and simple visual effects
+*/
+
+// a generic parent class for all VisualEffectSprites
 class VisualEffectSprite : public Sprite
 {
 public:
+	// constructor takes in a start position and an end position
 	VisualEffectSprite(Vector2f startPos, Vector2f endPos)
 		: Sprite(), m_StartPos(startPos), m_EndPos(endPos)
 	{
@@ -14,35 +24,45 @@ public:
 	{
 	}
 
+	// when the VEsprite resets, it returns to the start position
+	// and sets up the lerping to the end position once more
 	inline void Reset()
 	{
 		SetCentrePosition(m_StartPos);
 		LerpToPosition(m_EndPos);
 	}
 
+	// a new start and end position can also be specified when resetting
 	inline void Reset(Vector2f startPos, Vector2f endPos)
 	{
 		SetCentrePosition(startPos);
 		LerpToPosition(endPos);
 	}
 
+	// and then to qualify as a sprite the update function must be implemented
+	// and we also need the the Sprite::Update function to move this sprite across the screen
 	inline void Update(float deltaTime) override { Sprite::Update(deltaTime); };
 private:
+	// store the start and end position as vectors
 	Vector2f m_StartPos;
 	Vector2f m_EndPos;
 };
 
 
+// a carrot as visual effects for when the pet is eating
 class Carrot : public VisualEffectSprite
 {
 public:
+	// take in a start pos and an end pos
 	Carrot(Vector2f startPos, Vector2f endPos)
 		: VisualEffectSprite(startPos, endPos)
 	{
+		// set the image of the sprite to a carrot
 		SetImage(new Image(4, 3, R"(_\/_
 \  /
  \/ )"));
 
+		// resetting the sprite will move it into position and start the lerping
 		Reset();
 	}
 
@@ -51,13 +71,17 @@ public:
 	}
 };
 
+// a Z that comes out of the pet to show it is sleeping
 class SleepingZ : public VisualEffectSprite
 {
 public:
+	// take in a start and end pos
 	SleepingZ(Vector2f startPos, Vector2f endPos)
 		: VisualEffectSprite(startPos, endPos)
 	{
+		// set the image to simply a Z
 		SetImage(new Image(1, 1, "Z"));
+		// resetting the sprite will move it into position and start the lerping
 		Reset();
 	}
 
